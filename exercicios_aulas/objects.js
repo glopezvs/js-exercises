7ec7cf3f66c0d8f907fef74c5773fa5ce93d840e
glopezvs <83585720+glopezvs@users.noreply.github.com>
2024-02-02 19:27:13 +0000

    update esxercicios

--- exercicios_aulas/objects.js
+++ exercicios_aulas/objects.js
@@ -1,7 +1,9 @@
 const pessoa = {
     nome: "gabs",
     idade:"24",
-    naturalidade:"maracay"
+    naturalidade:"maracay",
+    0 : "boas",
+    "user-name": "tops"
 }
 const pessoa2 = new Object();
 pessoa2.nome = "gabs";
@@ -11,3 +13,7 @@ pessoa2.naturalidade ="maracay"
 console.log(Object.keys(pessoa2))
 console.log(Object.values(pessoa2))
 console.log(Object.entries(pessoa2))
+
+// o - nao é um indentificador valido nos nomes, porque é lido como "subtração"
+// teriamos qu usar "use-name" ou seja declarar como string e nao user-name
+// numeros sao propridades validas, mas ao ler temos que fazer por ex (pessoa[0])
